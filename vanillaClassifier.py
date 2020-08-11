970a4cfcaeab6891e3bf1f1d225e1135d43d34f8
Hamzza K <34022534+hamzza-K@users.noreply.github.com>
2020-08-11 23:17:20 +0500

    Update vanillaClassifier.py

--- vanillaClassifier.py
+++ vanillaClassifier.py
@@ -1,3 +1,61 @@
 
-def nothing():
-  pass
+import tensorflow as tf
+import tensorflow_dataset as tfds
+import numpy as np
+
+#Downloading and processing the data
+dataset, info = tfds.load('imdb_reviews/subwords8k', with_info=True,
+                          as_supervised=True)
+train_dataset, test_dataset = dataset['train'], dataset['test']
+
+BUFFER_SIZE = 10_000
+BATCH_SIZE = 64
+
+train_dataset = train_dataset.shuffle(BUFFER_SIZE)
+train_dataset = train_dataset.padded_batch(BATCH_SIZE)
+test_dataset = test_dataset.padded_batch(BATCH_SIZE)
+
+def pad_to_size(vec, size):
+  zeros = [0] * (size - len(vec))
+  vec.extend(zeros)
+  return vec
+
+encoder = info.features['text'].encoder
+
+
+def sample_predict(sample_pred_text, pad):
+  encoded_sample_pred_text = encoder.encode(sample_pred_text)
+
+  if pad:
+    encoded_sample_pred_text = pad_to_size(encoded_sample_pred_text, 64)
+  encoded_sample_pred_text = tf.cast(encoded_sample_pred_text, tf.float32)
+  predictions = model.predict(tf.expand_dims(encoded_sample_pred_text, 0))
+
+  return (predictions)
+
+
+# Building a simple Sequential Model 
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(encoder.vocab_size, 64),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64,  return_sequences=True)),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(1)
+])
+
+
+# Compiling the model
+model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              optimizer=tf.keras.optimizers.Adam(1e-4),
+              metrics=['accuracy'])
+
+# Training our model
+model.fit(train_dataset, epochs=5,
+                    validation_data=test_dataset,
+                    validation_steps=30)
+
+# Finally, saving our model
+model.save("my_model")
+
+
